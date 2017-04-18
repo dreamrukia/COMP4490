22058c53e780e2d60ffb03c8cc842b0614441c43
Jiaqing Zhu <dreamrukia@hotmail.com>
2017-04-18 18:45:47 -0500

    changed move script

--- Assets/script/move.cs
+++ Assets/script/move.cs
@@ -6,6 +6,7 @@ public class move : MonoBehaviour {
 
 	public float movementSpeed = 1.0f;
 	public float screenScale = 16.0f/9.0f;
+	public float speed = 100;
 
 	GameObject skybox;
 	// Use this for initialization
@@ -25,22 +26,33 @@ public class move : MonoBehaviour {
 
     void movement()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + movementSpeed );
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position = new Vector3(transform.position.x , transform.position.y, transform.position.z - movementSpeed );
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position = new Vector3(transform.position.x + movementSpeed * screenScale, transform.position.y, transform.position.z);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position = new Vector3(transform.position.x - movementSpeed * screenScale, transform.position.y, transform.position.z );
-        }
+//        if (Input.GetKey(KeyCode.W))
+//        {
+//            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + movementSpeed );
+//        }
+//        if (Input.GetKey(KeyCode.S))
+//        {
+//            transform.position = new Vector3(transform.position.x , transform.position.y, transform.position.z - movementSpeed );
+//        }
+//        if (Input.GetKey(KeyCode.D))
+//        {
+//            transform.position = new Vector3(transform.position.x + movementSpeed * screenScale, transform.position.y, transform.position.z);
+//        }
+//        if (Input.GetKey(KeyCode.A))
+//        {
+//            transform.position = new Vector3(transform.position.x - movementSpeed * screenScale, transform.position.y, transform.position.z );
+//        }
+		Camera cam = GameObject.Find("Camera").GetComponent<Camera>();
+				Vector3 face = cam.transform.forward;
+				face.y = 0.0f;
+				Rigidbody rb = GetComponent<Rigidbody> ();
+
+				
+		if (Input.GetKey (KeyCode.W)) {
+			rb.AddForce (face * speed);
+		} else if (Input.GetKey (KeyCode.S)) {
+			rb.AddForce (-face * speed);
+		}
     }
 
 
